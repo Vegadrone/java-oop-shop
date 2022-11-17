27978e8b769c444bacd99df24a45eff6c8efd678
Vegadrone <spenaantonino@gmail.com>
2022-11-17 15:18:54 +0100

    exercise complete

--- src/org/generation/italy/shop/Main.java
+++ src/org/generation/italy/shop/Main.java
@@ -1,5 +1,18 @@
 package org.generation.italy.shop;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		Prodotto prodotto1 = new Prodotto("Motosega ad aria", "Funziona ad aria! Come non lo so!", 300);
+		System.out.println(prodotto1.toString());
+		int prodotto1Prezzo = prodotto1.mostraPrezzo(); 
+		int prodotto1PrezzoIva = prodotto1.mostraPrezzoIva();
+		System.out.println("Prezzo: " + prodotto1Prezzo + " " + "€" + "\nPrezzo con Iva: " + prodotto1PrezzoIva + " " + "€");
+		
+		Prodotto prodotto2 = new Prodotto("BFG", "Big Fuckin Gun! E anche tu puoi uccidere demoni!", 15000);
+		System.out.println(prodotto2.toString());
+		int prodotto2Prezzo = prodotto2.mostraPrezzo(); 
+		int prodotto2PrezzoIva = prodotto2.mostraPrezzoIva();
+		System.out.println("Prezzo: " + prodotto2Prezzo + " " + "€" + "\nPrezzo con Iva: " + prodotto2PrezzoIva + " " + "€");
+	} 
 }
--- src/org/generation/italy/shop/Prodotto.java
+++ src/org/generation/italy/shop/Prodotto.java
@@ -1,5 +1,35 @@
 package org.generation.italy.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+	Random rnd = new Random();
+	
+	int codice;
+	int prezzo;
+	int iva = 20;
+	String nome;
+	String descrizione;
+	
+	public Prodotto (String nome, String descrizione, int prezzo){
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.codice = rnd.nextInt(10000, 99999);		
+	}
+	
+	public int mostraPrezzo() {
+		return prezzo;
+	}
+	
+	public int mostraPrezzoIva() {
+		return prezzo + (prezzo * iva /100);
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Nome: " + nome + "\nDescrizione: " + descrizione + "\nCodice: " + codice + "\nPrezzo: " + prezzo  + " " + "€";
+	}
 }
